65ab4023a8cdb8ad07fc3947af8c5787a97ac482
Ray C Horn <raychorn@gmail.com>
2021-10-07 15:43:35 -0600

    updates.

--- make_data_binary.py
+++ make_data_binary.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+import mujson as json
+
 import numba
 import numpy as np
 import pandas as pd
@@ -36,17 +38,18 @@ def frequency_analysis_of(data):
     return freqs
 
 
-def scatter_plot_list(data_list, title='***', xlabel='***', ylabel='***', xlim=None, ylim=None, save_path=None):
+def scatter_plot_list(data_list, freqs=None, image_fpath=None, title='***', xlabel='***', ylabel='***', xlim=None, ylim=None, save_path=None):
     rng = np.random.RandomState(0)
-    with timer.Timer() as timer1:
-        freq = frequency_analysis_of(data_list)
-    print('freq analysis: {} rows --> {:.2f} secs'.format(len(data_list), timer1.duration))
-    print('BEGIN: Freq.')
-    for k,v in freq.items():
+    if (freqs is None):
+        with timer.Timer() as timer1:
+            freqs = frequency_analysis_of(data_list)
+        print('freqs analysis: {} rows --> {:.2f} secs'.format(len(data_list), timer1.duration))
+    print('BEGIN: freqs.')
+    for k,v in freqs.items():
         print('{} : {}'.format(k, v))
-    print('END!!! Freq.')
-    x = [i for i in freq.keys()]
-    y = [i for i in freq.values()]
+    print('END!!! freqs.')
+    x = [i for i in freqs.keys()]
+    y = [i for i in freqs.values()]
     colors = rng.rand(len(x))
     sizes = [i*1000 for i in y]
 
@@ -58,7 +61,10 @@ def scatter_plot_list(data_list, title='***', xlabel='***', ylabel='***', xlim=N
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.show()
+    if (image_fpath is not None):
+        plt.savefig(image_fpath)
+    else:
+        plt.show()
 
 
 if (os.path.exists(data_file) and os.path.isfile(data_file)):
@@ -77,12 +83,30 @@ if (os.path.exists(data_file) and os.path.isfile(data_file)):
         protocols = df['protocol'].tolist()
     print('protocols: {} --> {} in {:.2f} secs'.format(len(protocols), protocols[:10], timer2.duration))
 
-    with timer.Timer() as timer3:
-        protocols_analysis = []
-        for protocol_seg in group_elements(10,protocols):
-            m = np.mean(normalize_numbers(protocol_seg), dtype=np.float32)
-            protocols_analysis.append(m)
-            #print('{} --> {}'.format(protocol_seg, m))
-    print('protocols_analysis: {} --> {} in {:.2f} secs'.format(len(protocols_analysis), protocols_analysis[:10], timer3.duration))
 
-    scatter_plot_list(protocols_analysis, title='protocols_analysis', xlabel='mean', ylabel='frequency', xlim=None, ylim=None, save_path=None)
+    charts_dirname = 'charts'
+
+    iCnt = 0
+    with timer.Timer() as timer0:
+        for chunk_size in range(2,102,1): # len(protocols)-2
+            with timer.Timer() as timer3:
+                protocols_analysis = []
+                for protocol_seg in group_elements(chunk_size,protocols):
+                    m = np.mean(normalize_numbers(protocol_seg), dtype=np.float32)
+                    protocols_analysis.append(m)
+            print('protocols_analysis: {} --> {} in {:.2f} secs'.format(len(protocols_analysis), protocols_analysis[:10], timer3.duration))
+
+            with timer.Timer() as timer1:
+                protocols_freqs = frequency_analysis_of(protocols_analysis)
+
+            ifpath = os.path.join(os.path.dirname(__file__), charts_dirname, 'sx-vpclogss3-filtered-09-25-2021-expanded-protocols-freqs-{}.png'.format(chunk_size))
+            scatter_plot_list(protocols_analysis, freqs=protocols_freqs, title='protocols_analysis', xlabel='mean', ylabel='frequency', image_fpath=ifpath, xlim=None, ylim=None, save_path=None)
+
+            jfpath = os.path.splitext(ifpath)[0] + '.json'
+            with open(jfpath, 'w') as f:
+                pfreqs = {'{:.2f}'.format(k):v for k,v in protocols_freqs.items()}
+                json.dump(pfreqs, f)
+
+            assert os.path.exists(ifpath) and os.path.isfile(ifpath), '{} does not exist or is not a file'.format(ifpath)
+        iCnt += 1
+    print('Analysis: {} iterations --> {:.2f} secs'.format(iCnt, timer0.duration))
