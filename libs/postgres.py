3ae1b6e39e90168f63d534577299168251ac5a63
Ray C Horn <raychorn@gmail.com>
2021-12-05 13:06:30 -0700

    updates.

---
+++ libs/postgres.py
@@ -0,0 +1,173 @@
+import os
+import sys
+
+import re
+
+import json
+
+import dotenv
+
+import datetime
+
+import traceback
+
+from contextlib import contextmanager
+
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+from sqlalchemy.orm import class_mapper
+
+@contextmanager
+def db_session(db_url_or_engine):
+    """ Creates a context with an open SQLAlchemy session.
+    """
+    engine = create_engine(db_url_or_engine) if isinstance(db_url_or_engine, str) else db_url_or_engine
+    connection = engine.connect()
+    db_session = scoped_session(sessionmaker(autocommit=False, autoflush=True, bind=engine))
+    try:
+        yield db_session
+    except:
+        db_session.rollback()
+        raise
+    finally:
+        db_session.close()
+        connection.close()
+
+class Query(object):
+    
+    def __init__(self, table_name, colName, targetValue, envPath=None, logger=None):
+        self.envPath = envPath
+        self.logger = logger
+        
+        self.table_name = table_name
+        self.colName = colName
+        self.targetValue = targetValue
+        
+        if ((self.envPath is None) or (not isinstance(self.envPath, str)) or (not os.path.exists(self.envPath)) or (not os.path.isfile(self.envPath))):
+            self.envPath = dotenv.find_dotenv()
+        assert (self.envPath is not None) and (isinstance(self.envPath, str)) and (os.path.exists(self.envPath)) and (os.path.isfile(self.envPath)), 'envPath ({}) is not valid or cannot be found.'.format(self.envPath)
+        dotenv.load_dotenv(self.envPath)
+
+        self.Base = declarative_base()
+
+        self.POSTGRES_DB_URI = os.environ.get('POSTGRES_DB_URI')
+        self.engine = create_engine(self.POSTGRES_DB_URI)
+
+        self.Base.metadata.create_all(self.engine)
+
+        self.metadata_obj = MetaData()
+        self.metadata_obj.reflect(bind=self.engine)
+
+        self.Table = Table(self.table_name, self.metadata_obj, autoload_with=self.engine)
+
+    def __call__(self, f):
+        try:
+            def wrapped_f(*args, **kwargs):
+                with db_session(self.engine) as session:
+                    kwargs['self'] = self
+                    kwargs['session'] = session
+                    f(*args, **kwargs)
+            return wrapped_f
+        except Exception as ex:
+            extype, ex, tb = sys.exc_info()
+            formatted = traceback.format_exception_only(extype, ex)[-1]
+            if (self.logger):
+                self.logger.error(formatted)
+            else:
+                print(formatted)
+
+def query_db(table_name, filter_by={}):
+    '''
+        filter_by = {
+        "column1": "!1", # not equal to
+        "column2": "1",   # equal to
+        "column3": ">1",  # great to. etc...
+        }
+    '''
+    def computed_operator(column, v):
+        if re.match(r"^!", v):
+            """__ne__"""
+            val = re.sub(r"!", "", v)
+            return column.__ne__(val)
+        if re.match(r">(?!=)", v):
+            """__gt__"""
+            val = re.sub(r">(?!=)", "", v)
+            return column.__gt__(val)
+        if re.match(r"<(?!=)", v):
+            """__lt__"""
+            val = re.sub(r"<(?!=)", "", v)
+            return column.__lt__(val)
+        if re.match(r">=", v):
+            """__ge__"""
+            val = re.sub(r">=", "", v)
+            return column.__ge__(val)
+        if re.match(r"<=", v):
+            """__le__"""
+            val = re.sub(r"<=", "", v)
+            return column.__le__(val)
+        if re.match(r"(\w*),(\w*)", v):
+            """between"""
+            a, b = re.split(r",", v)
+            return column.between(a, b)
+        """ default __eq__ """
+        return column.__eq__(v)
+
+    
+    fp_env = dotenv.find_dotenv()
+    print('fp_env: {}'.format(fp_env))
+    dotenv.load_dotenv(fp_env)
+
+    Base = declarative_base()
+
+    POSTGRES_DB_URI = os.environ.get('POSTGRES_DB_URI')
+    engine = create_engine(POSTGRES_DB_URI)
+
+    Base.metadata.create_all(engine)
+
+    metadata_obj = MetaData()
+    metadata_obj.reflect(bind=engine)
+
+    with db_session(engine) as session:
+        theTable = Table(table_name, metadata_obj, autoload_with=engine)
+        
+        query = session.query()
+        filters = []
+        theCols = theTable.columns
+        mapped_cols = {c.name:c for c in theTable.columns}
+        for k, v in filter_by.items():
+            if (not k in list(mapped_cols.keys())):
+                continue
+            filters.append(computed_operator(mapped_cols.get(k), "{}".format(v)))
+        query = query.filter(*filters)
+        result = query.all()
+        
+        if (type(result).__name__ == 'Row'):
+            _result = dict(zip(theTable.columns.keys(), result))
+            print('_result: {}'.format(_result))
+
+def isgoodipv4(s):
+    pieces = s.split('.')
+    if (len(pieces) != 4):
+        return False
+    try: 
+        return all(0<=int(p)<256 for p in pieces)
+    except ValueError:
+        return False
+    return False
+
+if (__name__ == '__main__'):
+    @Query('asset', 'hostname', '172.31.60.14')
+    def foo(self=None, session=None):
+        _result = None
+        result = session.query(self.Table).filter(self.Table.c.hostname=='{}'.format(self.targetValue)).one()
+        if (type(result).__name__ == 'Row'):
+            _result = dict(zip(self.Table.columns.keys(), result))
+        print('results: {}'.format(_result))
+    foo()
+    print()
--- postgres/securex_assets_reader.py
+++ postgres/securex_assets_reader.py
@@ -1,13 +1,24 @@
 import os
 import sys
 
+import json
+
+import dotenv
+
+import datetime
+
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 
+fp_env = dotenv.find_dotenv()
+print('fp_env: {}'.format(fp_env))
+dotenv.load_dotenv(fp_env)
+
 Base = declarative_base()
 
-DB_URI = 'postgresql+psycopg2://securexdev:secureme2020@tp01-2066.web-service.org:49153/securex_assets'
-engine = create_engine(DB_URI)
+POSTGRES_DB_URI = os.environ.get('POSTGRES_DB_URI')
+
+engine = create_engine(POSTGRES_DB_URI)
 
 tables = engine.table_names()
 print(tables)
@@ -15,6 +26,16 @@ print(tables)
 from contextlib import contextmanager
 
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+def isgoodipv4(s):
+    pieces = s.split('.')
+    if (len(pieces) != 4):
+        return False
+    try: 
+        return all(0<=int(p)<256 for p in pieces)
+    except ValueError:
+        return False
+    return False
 
 @contextmanager
 def db_session(db_url):
@@ -37,22 +58,46 @@ Base.metadata.create_all(engine)
 metadata_obj = MetaData()
 metadata_obj.reflect(bind=engine)
 
-with db_session(DB_URI) as session:
+interested_in = ['asset']
+
+with db_session(POSTGRES_DB_URI) as session:
     with open('{}/securex_assets_report.txt'.format(os.path.dirname(__file__)), 'w') as fOut:
         _tables = sorted([t for t in tables])
         for t in _tables:
-            print(t)
+            if (not t in interested_in):
+                continue
+            print('{} -> {}'.format(t, type(t)))
             print(t, file=fOut)
             
             theTable = Table(t, metadata_obj, autoload_with=engine)
             
             print('BEGIN: theTable.columns', file=fOut)
             for c in theTable.columns:
-                print('{} :: {}'.format(t, c), file=fOut)
+                msg = '{} :: {}'.format(t, c)
+                print(msg)
+                print(msg, file=fOut)
             print('END!!! theTable.columns', file=fOut)
             
-            #print(session.query(aTable).all())
-            print('-'*80, file=fOut)
+            items = session.query(theTable).all()
+            fpathJson = '{}/securex_postgres_{}.json'.format(os.path.dirname(__file__), t)
+            with open(fpathJson, 'w') as fJson1:
+                _items = [list(r) for r in items]
+                _items = [dict(zip(theTable.columns.keys(), r)) for r in _items]
+                for row in _items:
+                    for k,v in row.items():
+                        if (isinstance(v, datetime.datetime)):
+                            row[k] = v.isoformat()
+                    ip_addr = row.get('hostname', '')
+                    if (isgoodipv4(ip_addr)):
+                        result = session.query(theTable).filter(theTable.c.hostname=='{}'.format(ip_addr)).one()
+                        if (type(result).__name__ == 'Row'):
+                            _result = dict(zip(theTable.columns.keys(), result))
+                            assert _result.get('hostname') == ip_addr, '{} != {}'.format(_result.get('hostname'), ip_addr)
+                json.dump(_items, fJson1, indent=4)
+            print('{} items written to {}'.format(len(items), fpathJson))
+            msg = '-'*80
+            print(msg)
+            print(msg, file=fOut)
             print('\n', file=fOut)
             print()
         
